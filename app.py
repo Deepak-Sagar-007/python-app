a063dd6d8229e75bd6853b41877e3264e9d61313
Deepak Sagar <113299486+Deepak-Sagar-007@users.noreply.github.com>
2026-07-19 21:15:41 +0530

    Correct greeting from 'Hello' to 'Helllo'
    Fix typo in greeting message.

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 
 def main():
     name = os.getenv("NAME", "DefaultUser")
-    print(f"Hello {name}, how are you ")
+    print(f"Helllo {name}, how are you ")
 
 if __name__ == "__main__":
     main()
